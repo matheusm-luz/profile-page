94b62629f04c799faf3e62666752d83bbaa4d8a0
Matheus Monteiro <matheusmonteiro.luz@outlook.com>
2024-09-24 08:05:46 -0300

    Commit test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,3 @@
-// import { Component } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
-
-// @Component({
-//   selector: 'app-root',
-//   standalone: true,
-//   imports: [RouterOutlet],
-//   templateUrl: './app.component.html',
-//   styleUrl: './app.component.css'
-// })
-// export class AppComponent {
-//   title = 'profile-page';
-// }
-
 import { Component } from '@angular/core';
 
 @Component({
