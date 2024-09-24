383d3930ae660391f8b71bef32e338eca1e88637
Matheus Monteiro <matheusmonteiro.luz@outlook.com>
2024-09-24 08:05:08 -0300

    Commit test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
+// import { Component } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [RouterOutlet],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css'
+// })
+// export class AppComponent {
+//   title = 'profile-page';
+// }
+
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'profile-page';
+
+  // Exemplo de ação ao clicar em um botão
+  viewWork() {
+    alert('Você será redirecionado para os projetos!');
+  }
 }
